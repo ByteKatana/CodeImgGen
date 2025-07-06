20379b9326eb5012afc6a6df5ed6f14c06dfc178
ByteKatana <19264901+ByteKatana@users.noreply.github.com>
2025-07-06 16:07:42 +0300

    ✨ feat: add `CodeDisplay` component to `App`

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,44 @@
 import Navbar from "@/components/Navbar.tsx"
 import EditorWrapper from "@/components/editor-wrapper.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { useState } from "react"
+import type { CodeResponse } from "@/types.ts"
+import { EditorContextProvider } from "@/hooks/use-editor-context.tsx"
+import CodeDisplay from "./components/code-display"
 
 function App() {
+  const [context, setContext] = useState<CodeResponse | null>(null)
+  const [contextError, setContextError] = useState<string | null>(null)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <>
         <Navbar />
         <div id="container" className="flex min-h-screen items-center justify-center p-5">
           <div className="flex flex-col w-full gap-4 bg-background rounded-md shadow-md dark:bg-background-dark">
+            {isLoading && <div className="flex justify-center">Loading...</div>}
+            {contextError && (
+              <div className="flex justify-center">
+                <b>Error:</b> {contextError}
+              </div>
+            )}
+            {context && (
+              <CodeDisplay
+                highlightedCode={context.data?.code ?? ""}
+                styleDefinitions={context.data?.style_definitions ?? ""}
+                backgroundColor={context.data?.style_bg_color ?? ""}
+              />
+            )}
             <input
               type="text"
               placeholder="Name your file"
               className=" text-3xl font-light ml-5 mr-5 p-10 border-b-2 border-neutral-300 cursor-text focus:outline-none focus:border-rose-600 hover:border-rose-600 transition-colors duration-500 ease-in-out animate-slowBlink"
             />
-            <EditorWrapper />
+            <EditorContextProvider
+              value={{ context, setContext, contextError, setContextError, isLoading, setIsLoading }}>
+              <EditorWrapper />
+            </EditorContextProvider>
           </div>
         </div>
       </>
