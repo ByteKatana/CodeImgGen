6e1a5309868916f5fc62184d30bbe3f1544c0d7d
ByteKatana <19264901+ByteKatana@users.noreply.github.com>
2025-07-06 16:09:44 +0300

    ✨ feat: add `GenerateButtonPlugin` for editor toolbar integration

---
+++ frontend/src/components/editor/plugins/toolbar/generate-button-plugin.tsx
@@ -0,0 +1,53 @@
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
+import { generateCode } from "@/api.ts"
+import { $getRoot } from "lexical"
+import { BiImageAdd } from "react-icons/bi"
+import { Button } from "@/components/ui/button.tsx"
+import { useEditorContext } from "@/hooks/use-editor-context.tsx"
+import type { FormEvent } from "react"
+
+export function GenerateButtonPlugin() {
+  const [editor] = useLexicalComposerContext()
+  const { context, setContext, contextError, setContextError, isLoading, setIsLoading } = useEditorContext()
+
+  const handleSubmit = async (e: FormEvent) => {
+    // Get the plaintext (or use editorState.read/serialize for rich text, JSON, or HTML)
+    e.preventDefault()
+    editor.update(() => {
+      const root = $getRoot()
+      const textContent = root.getTextContent() // Plain text
+
+      // Or for rich content:
+      // const json = editor.getEditorState().toJSON();
+
+      setIsLoading(true)
+
+      // Submit to API
+      generateCode(textContent)
+        .then((res) => {
+          // handle success
+          setContext(res)
+        })
+        .catch((err: unknown) => {
+          // handle error
+          setContextError(err)
+        })
+        .finally(() => {
+          setIsLoading(false)
+        })
+    })
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Button
+        className="hover:bg-green-400 hover:text-neutral-50 dark:hover:bg-green-600 "
+        variant="outline"
+        type="submit"
+        size="sm">
+        <BiImageAdd />
+        {isLoading ? "Generating..." : "Generate"}
+      </Button>
+    </form>
+  )
+}
