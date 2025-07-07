8aa9eabd3ba551cc57a19bee70e5474dba65ad08
ByteKatana <19264901+ByteKatana@users.noreply.github.com>
2025-07-07 16:22:17 +0300

    ♻️ refactor: wrap `CodeDisplay` component with the `EditorContextProvider` for the `Back to Editor` functionality to work

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,27 +17,30 @@ function App() {
         <Navbar />
         <div id="container" className="flex min-h-screen items-center justify-center p-5">
           <div className="flex flex-col w-full gap-4 bg-background rounded-md shadow-md dark:bg-background-dark">
-            {isLoading && <div className="flex justify-center">Loading...</div>}
-            {contextError && (
-              <div className="flex justify-center">
-                <b>Error:</b> {contextError}
-              </div>
-            )}
-            {context && (
-              <CodeDisplay
-                highlightedCode={context.data?.code ?? ""}
-                styleDefinitions={context.data?.style_definitions ?? ""}
-                backgroundColor={context.data?.style_bg_color ?? ""}
-              />
-            )}
-            <input
-              type="text"
-              placeholder="Name your file"
-              className=" text-3xl font-light ml-5 mr-5 p-10 border-b-2 border-neutral-300 cursor-text focus:outline-none focus:border-rose-600 hover:border-rose-600 transition-colors duration-500 ease-in-out animate-slowBlink"
-            />
             <EditorContextProvider
               value={{ context, setContext, contextError, setContextError, isLoading, setIsLoading }}>
-              <EditorWrapper />
+              {isLoading && <div className="flex justify-center">Loading...</div>}
+              {contextError && (
+                <div className="flex justify-center">
+                  <b>Error:</b> {contextError}
+                </div>
+              )}
+              {context && (
+                <CodeDisplay
+                  highlightedCode={context.data?.code ?? ""}
+                  styleDefinitions={context.data?.style_definitions ?? ""}
+                  backgroundColor={context.data?.style_bg_color ?? ""}
+                />
+              )}
+              <div className={`${context ? "hidden" : "flex flex-col"}`}>
+                <input
+                  type="text"
+                  placeholder="Name your file"
+                  className=" text-3xl font-light ml-5 mr-5 p-10 border-b-2 border-neutral-300 cursor-text focus:outline-none focus:border-rose-600 hover:border-rose-600 transition-colors duration-500 ease-in-out animate-slowBlink"
+                />
+
+                <EditorWrapper />
+              </div>
             </EditorContextProvider>
           </div>
         </div>
