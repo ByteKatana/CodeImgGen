fbc17491197239994f5dca4883588f614f891228
ByteKatana <19264901+ByteKatana@users.noreply.github.com>
2025-07-07 16:15:52 +0300

    ♻️ refactor: add "Back to Editor" action and improve Preview card layout in `CodeDisplay` component

--- frontend/src/components/code-display.tsx
+++ frontend/src/components/code-display.tsx
@@ -2,7 +2,7 @@ import React, { type FormEvent, useState } from "react"
 import styled, { createGlobalStyle } from "styled-components"
 import type { CodeDisplayProps, ImageResponse } from "@/types.ts"
 import { Button } from "@/components/ui/button.tsx"
-import { BiDownload, BiImageAdd } from "react-icons/bi"
+import { BiCodeBlock, BiDownload, BiImageAdd } from "react-icons/bi"
 import {
   Card,
   CardAction,
@@ -13,6 +13,7 @@ import {
   CardTitle
 } from "@/components/ui/card.tsx"
 import { takeScreenshotOfMyCode } from "@/lib/screenshot.ts"
+import { useEditorContext } from "@/hooks/use-editor-context.tsx"
 
 const PygmentsStyles = createGlobalStyle<{ styleDefinitions: string }>`
   ${(props) => props.styleDefinitions}
@@ -30,6 +31,7 @@ const CodeDisplay: React.FC<CodeDisplayProps> = ({ highlightedCode, styleDefinit
   const [imgGenError, setImgGenError] = useState<string | null>(null)
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [imgUrl, setImgUrl] = useState<string | null>(null)
+  const { setContext } = useEditorContext()
 
   const handleScreenshot = async (e: FormEvent) => {
     e.preventDefault()
@@ -38,6 +40,13 @@ const CodeDisplay: React.FC<CodeDisplayProps> = ({ highlightedCode, styleDefinit
       console.log("Screenshot URL:", imgUrl)
       setImgUrl(imgUrl)
     }
+  }
+
+  const backToEditor = () => {
+    setContext(null)
+    setImgUrl(null)
+    setImgGenError(null)
+    setIsLoading(false)
   }
 
   return (
@@ -50,16 +59,27 @@ const CodeDisplay: React.FC<CodeDisplayProps> = ({ highlightedCode, styleDefinit
             <CardTitle>Your Image is Ready 🎉!</CardTitle>
             <CardDescription>You can download your image now...</CardDescription>
             <CardAction>
-              <Button
-                className="bg-green-500 text-neutral-50 hover:bg-slate-50 hover:text-green-600 border-1 border-background hover:border-green-600"
-                variant="link"
-                type="button"
-                size="lg">
-                <BiDownload />
-                <a href={imgUrl ?? "http://localhost:5173/error_window.png"} download="screenshot.png">
-                  Download!
-                </a>
-              </Button>
+              <div className="flex flex-row">
+                <Button
+                  className="bg-rose-600 text-neutral-50 hover:bg-slate-50 hover:text-rose-600 border-1 border-background hover:border-rose-600"
+                  variant="link"
+                  type="button"
+                  onClick={() => backToEditor()}
+                  size="lg">
+                  <BiCodeBlock />
+                  Back to Editor
+                </Button>
+                <Button
+                  className="bg-green-500 text-neutral-50 hover:bg-slate-50 hover:text-green-600 border-1 border-background hover:border-green-600"
+                  variant="link"
+                  type="button"
+                  size="lg">
+                  <BiDownload />
+                  <a href={imgUrl ?? "http://localhost:5173/error_window.png"} download="screenshot.png">
+                    Download!
+                  </a>
+                </Button>
+              </div>
             </CardAction>
           </CardHeader>
           <CardContent>
@@ -73,20 +93,43 @@ const CodeDisplay: React.FC<CodeDisplayProps> = ({ highlightedCode, styleDefinit
       <div
         className={`px-5 py-10 mt-20 ${imgUrl ? "hidden" : "flex flex-col"} transition-opacity duration-500 ease-in-out `}>
         <h1 className="text-3xl font-semibold text-rose-800 mb-2">Preview</h1>
-        <form onSubmit={handleScreenshot}>
-          <Button
-            className="hover:bg-green-400 hover:text-neutral-50 dark:hover:bg-green-600 "
-            variant="outline"
-            type="submit"
-            size="lg">
-            <BiImageAdd />
-            Create Image
-          </Button>
-        </form>
-        <div className="my_code">
-          <PygmentsStyles styleDefinitions={styleDefinitions} />
-          <CodeContainer backgroundColor={backgroundColor} dangerouslySetInnerHTML={{ __html: highlightedCode }} />
-        </div>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Preview of your image 🎉!</CardTitle>
+            <CardDescription>Here is how it's look like...</CardDescription>
+            <CardAction>
+              <div className="flex flex-row">
+                <Button
+                  className="bg-rose-600 text-neutral-50 hover:bg-slate-50 hover:text-rose-600 border-1 border-background hover:border-rose-600"
+                  variant="link"
+                  type="button"
+                  onClick={() => backToEditor()}
+                  size="lg">
+                  <BiCodeBlock />
+                  Back to Editor
+                </Button>
+                <form onSubmit={handleScreenshot}>
+                  <Button
+                    className="hover:bg-green-400 hover:text-neutral-50 dark:hover:bg-green-600 "
+                    variant="outline"
+                    type="submit"
+                    size="lg">
+                    <BiImageAdd />
+                    Create Image
+                  </Button>
+                </form>
+              </div>
+            </CardAction>
+          </CardHeader>
+          <CardContent>
+            <div className="my_code">
+              <PygmentsStyles styleDefinitions={styleDefinitions} />
+              <CodeContainer backgroundColor={backgroundColor} dangerouslySetInnerHTML={{ __html: highlightedCode }} />
+            </div>
+          </CardContent>
+          <CardFooter></CardFooter>
+        </Card>
       </div>
     </>
   )
